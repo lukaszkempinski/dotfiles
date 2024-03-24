61fab8459aab02f5f903533e97a6436731367070
Lukasz Kempinski <lukasz.kempinski@dxc.com>
2024-03-24 21:43:37 +0100

    Add install scripts and terminal config

---
+++ brew.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install Homebrew if it isn't already installed
+if ! command -v brew &>/dev/null; then
+    echo "Homebrew not installed. Installing Homebrew."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Attempt to set up Homebrew PATH automatically for this session
+    if [ -x "/opt/homebrew/bin/brew" ]; then
+        # For Apple Silicon Macs
+        echo "Configuring Homebrew in PATH for Apple Silicon Mac..."
+        export PATH="/opt/homebrew/bin:$PATH"
+    fi
+else
+    echo "Homebrew is already installed."
+fi
